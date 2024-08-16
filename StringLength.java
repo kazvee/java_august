663d1116b6d4d2134a6e451465a0e45679c4cccc
kazvee <kazveedev@gmail.com>
2024-08-16 13:51:28 -0600

    Add StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,11 @@
+class StringLength {
+  public static void main(String[] args) {
+    // This String object contains the name Tina
+    String name = "Tina";
+    // The length() method can be used to get the length
+    // (number of characters) in a String.
+    int length = name.length();
+    System.out.println("The name " + name + " has a length of " +
+        length + ".");
+  }
+}
