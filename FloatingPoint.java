9d478cbb45f73a14a183047b7d0fdb577b8c3fa1
kazvee <kazveedev@gmail.com>
2024-08-16 12:42:44 -0600

    Add FloatingPoint.java

---
+++ FloatingPoint.java
@@ -0,0 +1,8 @@
+public class FloatingPoint {
+  public static void main(String[] args) {
+    float sampleFloatValue = 0.123456789f;
+    double sampleDoubleValue = 0.123456789;
+    System.out.println("Value as float: " + sampleFloatValue);
+    System.out.println("Value as double: " + sampleDoubleValue);
+  }
+}
