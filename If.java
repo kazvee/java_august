68ea7f240828a9a7a135db028c6ef755655a8a5d
kazvee <kazveedev@gmail.com>
2024-08-16 15:23:23 -0600

    Add If.java

---
+++ If.java
@@ -0,0 +1,8 @@
+class If {
+  public static void main(String[] args) {
+    float temperature = 82.0f;
+    if (temperature > 0) {
+      System.out.println("Temperature is positive.");
+    }
+  }
+}
