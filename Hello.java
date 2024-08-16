5a087745ef7b58d3227cc41f7cd87ab1b3696144
kazvee <kazveedev@gmail.com>
2024-08-16 12:22:01 -0600

    Add Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+  public static void main(String[] args) {
+    // This code prints out the message 3 times.
+    System.out.println("1. Hello, world!"); // 1st output
+    // System.out.println("2. Hello, world!"); // 2nd output
+    System.out.println("3. Hello, world!"); // 3rd output
+  }
+}
