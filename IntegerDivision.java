371426fded6f312ef639b770013209b2edbc792b
kazvee <kazveedev@gmail.com>
2024-08-16 13:08:23 -0600

    Add IntegerDivision.java

---
+++ IntegerDivision.java
@@ -0,0 +1,15 @@
+public class IntegerDivision {
+  public static void main(String[] args) {
+    int two = 2;
+    int three = 3;
+    int four = 4;
+    System.out.println("Integer Division Examples: ");
+    int integerQuotient = three / two;
+    // The + is for concatenation not addition
+    System.out.println("\t3 / 2 = " + integerQuotient);
+    integerQuotient = four / two;
+    System.out.println("\t4 / 2 = " + integerQuotient);
+    integerQuotient = two / three;
+    System.out.println("\t2 / 3 = " + integerQuotient);
+  }
+}
