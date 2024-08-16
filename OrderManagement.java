d5047894bb1fe5768819e6bb7fb4c3a851478f42
kazvee <kazveedev@gmail.com>
2024-08-16 12:40:51 -0600

    Add OrderManagement.java

---
+++ OrderManagement.java
@@ -0,0 +1,10 @@
+public class OrderManagement {
+  public static void main(String[] args) {
+    // Declare variables & initialize them
+    int itemCount = 3;
+    String customerName = "Jones, Anne";
+    // Display values
+    System.out.println("The customer's name is " + customerName + ".");
+    System.out.println("The order contains " + itemCount + " item(s).");
+  }
+}
