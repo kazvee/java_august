e86480909f0da164dac0621ad18b513fe3d95ddc
kazvee <kazveedev@gmail.com>
2024-08-16 14:15:38 -0600

    Add ConsolePrint.java

---
+++ ConsolePrint.java
@@ -0,0 +1,10 @@
+class ConsolePrint {
+  public static void main(String[] args) {
+    // println() adds \n at the end of each output
+    System.out.println("Output 1");
+    System.out.println("Output 2");
+    // print() does not add \n
+    System.out.print("Output 3");
+    System.out.print("Output 4");
+  }
+}
