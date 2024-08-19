c0d7ac5b86ccebed647277f8aa0f69ff9f27bbe1
kazvee <kazveedev@gmail.com>
2024-08-18 21:06:18 -0600

    Add NumbersArray.java

---
+++ Unit_2/NumbersArray.java
@@ -0,0 +1,8 @@
+class NumbersArray {
+  public static void main(String[] args) {
+    int[] numbers = {17, 123};
+    System.out.println(numbers[0] + ", " + numbers[1]);
+    numbers[1] = 5;
+    System.out.println(numbers[0] + ", " + numbers[1]);
+  }
+}
