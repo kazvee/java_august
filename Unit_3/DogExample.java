5ee576faa6ff70a398e1a8e086393f9cf50465fc
kazvee <kazveedev@gmail.com>
2024-08-24 08:27:40 -0600

    Add DogExample.java

---
+++ Unit_3/DogExample.java
@@ -0,0 +1,18 @@
+public class DogExample {
+  public static void main(String[] args) {
+    // Call constructor using keyword new.
+    // Assign new Dog object to variable myDog.
+    // Class names starts with capital letter
+    // but variable name starts with lowercase
+    Dog myDog = new Dog("Fluffy", "Beagle", 2, "Brown");
+    // Call "get" methods to access data.
+    // All methods called via the Dog object created above
+    System.out.println("Name: " + myDog.getName());
+    System.out.println("Breed: " + myDog.getBreed());
+    System.out.println("Age: " + myDog.getAge());
+    System.out.println("Color: " + myDog.getColor());
+    System.out.print(myDog.getName() + " says ");
+    // Call bark() method
+    myDog.bark();
+  }
+}
