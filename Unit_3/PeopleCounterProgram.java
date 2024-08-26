eedfc217080de0b8be1bc166877e560fd3ba6820
kazvee <kazveedev@gmail.com>
2024-08-25 18:45:17 -0600

    Add PeopleCounterProgram.java

---
+++ Unit_3/PeopleCounterProgram.java
@@ -0,0 +1,12 @@
+class PeopleCounterProgram {
+  public static void main(String[] args) {
+    // Construct a PeopleCounter
+    PeopleCounter pc = new PeopleCounter();
+    // Count 5 people one by one
+    pc.anotherOne();
+    pc.anotherOne();
+    pc.anotherOne();
+    pc.anotherOne();
+    pc.anotherOne();
+  }
+}
