b01d42aa999e235a9705c271b3a687e4dcb36602
kazvee <kazveedev@gmail.com>
2024-08-27 08:30:39 -0600

    Update MemberInheritance.java

--- Unit_3/MemberInheritance.java
+++ Unit_3/MemberInheritance.java
@@ -4,15 +4,20 @@ public class MemberInheritance {
         System.out.println("First Name: " + testMember.getFirstName());
         System.out.println("Last Name: " + testMember.getLastName());
         System.out.println("Exp. Date: " + testMember.getExpiryDate());
+        System.out.println(testMember.getStatus());
+        System.out.println("--------------------");
 
-        Admin testAdmin = new Admin("root", "admin");
+        Admin testAdmin = new Admin("root", "admin", "ABRACADABRA");
         System.out.println("First Name: " + testAdmin.getFirstName());
         System.out.println("Last Name: " + testAdmin.getLastName());
         System.out.println("Exp. Date: " + testAdmin.getExpiryDate());
+        System.out.println(testAdmin.getStatus());
+        System.out.println("--------------------");
 
         User testUser = new User("Amy", "Smith");
         System.out.println("First Name: " + testUser.getFirstName());
         System.out.println("Last Name: " + testUser.getLastName());
         System.out.println("Exp. Date: " + testUser.getExpiryDate());
+        System.out.println(testUser.getStatus());
     }
 }
