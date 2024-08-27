f4492d8121eb85882875b10b079cebbb4137761c
kazvee <kazveedev@gmail.com>
2024-08-27 08:30:06 -0600

    Update User.java

--- Unit_3/User.java
+++ Unit_3/User.java
@@ -2,4 +2,8 @@ public class User extends Member {
     public User(String firstName, String lastName) {
         super(firstName, lastName);
     }
+
+    public String getStatus() {
+        return getFirstName() + " " + getLastName() + " is a User.";
+    }
 }
